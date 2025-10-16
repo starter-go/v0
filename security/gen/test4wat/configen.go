d6e0b92dd3cb5e3c8500079b5fa2a4831aedce03
kge <kge@bitwormhole.com>
2025-10-16 16:47:47 +0800

    创建了新的 go 项目 : 'security'

---
+++ security/gen/test4wat/configen.go
@@ -0,0 +1,9 @@
+package test4wat
+
+import "github.com/starter-go/application"
+
+//starter:configen(version="4")
+
+func ExportComponents(cr application.ComponentRegistry) error {
+	return registerComponents(cr)
+}
