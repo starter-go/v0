5bc26d9f87c84b58a65de8626b89b0fee721b841
kge <kge@bitwormhole.com>
2026-05-26 09:20:42 +0800

    up to 'subjects/v0.0.4'

--- subjects/module_t.go
+++ subjects/module_t.go
@@ -10,8 +10,8 @@ import (
 
 const (
 	theModuleName     = "github.com/starter-go/v0/subjects"
-	theModuleVersion  = "v0.0.3"
-	theModuleRevision = 3
+	theModuleVersion  = "v0.0.4"
+	theModuleRevision = 4
 )
 
 ////////////////////////////////////////////////////////////////////////////////
