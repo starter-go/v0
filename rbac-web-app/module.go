ea21e4c8e95060e91f3b0a4ce2adb53603d79293
kge <kge@bitwormhole.com>
2026-07-18 22:20:56 +0800

    rbac-wa : update to v0.10.2

--- rbac-web-app/module.go
+++ rbac-web-app/module.go
@@ -10,8 +10,8 @@ import (
 
 const (
 	theModuleName     = "github.com/starter-go/v0/rbac-web-app"
-	theModuleVersion  = "v0.10.1"
-	theModuleRevision = 5
+	theModuleVersion  = "v0.10.2"
+	theModuleRevision = 6
 )
 
 ////////////////////////////////////////////////////////////////////////////////
